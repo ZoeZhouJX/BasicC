6efd3eefe7f0ecf6cf2d8a800ec3ece78184330a
zjx <zjx@163.com>
2023-12-11 19:19:39 +0800

    [新增]makefile

--- demoMakefile/main.c
+++ demoMakefile/main.c
@@ -1,31 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-int printString()
-{
-    int ret = 0;
-    printf("print string\n");
-    return ret;
-}
 
-int selectChoice(int choice)
-{
-    switch(choice)
-    {
-        case 1:
-        printf("print 1\n");
-        break;
-
-        case 2:
-        printf("print 2\n");
-        break;
-
-        default:
-        break;
-    }
-}
 int main()
 {
-    int num1 = 10;
-    printf("num:%d\n",num1);
+    printf("hello world\n");
     return 0;
 }
